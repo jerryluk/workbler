940876eb5c5ed6c74e36c040f41a34fea22f3d33
Jerry Luk <jerry@eva.gateway.2wire.net>
2010-01-20 10:21:14 -0800

    Reorder the background tasks

--- generators/workbler/templates/config/startup.rb
+++ generators/workbler/templates/config/startup.rb
@@ -5,10 +5,12 @@ JRuby::Rack::RailsServletHelper.instance.load_environment
 # Tasks to run before starting up Rails server (e.g. migrations)
 ActiveRecord::Migrator.migrate("#{RAILS_ROOT}/db/migrate/", nil)
 
-# Background Tasks
+Thread.new do |t|
+  require 'workble'
+  Thread.stop
+end
+
+# Other Background Tasks
 Thread.new do |t|
 
 end
-
-require 'workble'
-Thread.stop
